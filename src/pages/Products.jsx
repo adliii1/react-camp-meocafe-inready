e9ab84c3b0d9bcd89aa9435779143dfdb0b71819
adliii1 <muslikhahamd8@gmail.com>
2024-10-08 17:41:10 +0700

    deploy

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,34 @@
 import { Link } from "react-router-dom";
 import Button from "../components/ui/Button";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Products = () => {
+	const [data, setData] = useState([]);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+  				const respons = await axios.get("http://localhost:8000/products");
+				setData(respons.data);
+			} catch (error) {
+				setError(error.message)
+			}
+		}
+
+		fetchData();
+	}, [data])
+
+	const deleteData = async (id) => {
+		try {
+			const response = await axios.delete(`http://localhost:8000/products/${id}`);
+				location.reload();
+		} catch (err) {
+			setError(err.message);
+		}
+	}
+
 	return (
 		<>
 			<div className="flex justify-between items-center">
@@ -25,180 +52,37 @@ const Products = () => {
 						</tr>
 					</thead>
 					<tbody className="table-row-group font-semibold">
-						<tr className="border-borderPrimary border-b">
-							<td className="py-6 flex justify-center">
-								<div className="w-14 overflow-hidden rounded-xl">
-									<img
-										src="https://images.unsplash.com/photo-1569718212165-3a8278d5f624?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFtZW58ZW58MHx8MHx8fDA%3D"
-										alt=""
-									/>
-								</div>
-							</td>
-							<td>Fried Rice</td>
-							<td>Food</td>
-							<td>Rp25.000</td>
-							<td>10</td>
-							<td className="h-full">
-								<div className="inline-flex rounded-lg font-normal bg-[#FAFBFD] border border-borderPrimary">
-									<Link
-										to="editProduct/123"
-										className="py-2 px-4 text-black"
-										aria-label="edit"
-									>
-										<i className="ri-edit-box-line"></i>
-									</Link>
-									<div className="w-[1px] border-borderPrimary h-10"></div>
-									<button className="py-2 px-4 text-error" aria-label="delete">
-										<i className="ri-delete-bin-line"></i>
-									</button>
-								</div>
-							</td>
-						</tr>
-						<tr className="border-borderPrimary border-b">
-							<td className="py-6 flex justify-center">
-								<div className="w-14 overflow-hidden rounded-xl">
-									<img
-										src="https://images.unsplash.com/photo-1569718212165-3a8278d5f624?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFtZW58ZW58MHx8MHx8fDA%3D"
-										alt=""
-									/>
-								</div>
-							</td>
-							<td>Fried Rice</td>
-							<td>Food</td>
-							<td>Rp25.000</td>
-							<td>10</td>
-							<td className="h-full">
-								<div className="inline-flex rounded-lg font-normal bg-[#FAFBFD] border border-borderPrimary">
-									<a
-										href="./editProduct.html"
-										className="py-2 px-4 text-black"
-										aria-label="edit"
-									>
-										<i className="ri-edit-box-line"></i>
-									</a>
-									<div className="w-[1px] border-borderPrimary h-10"></div>
-									<button className="py-2 px-4 text-error" aria-label="delete">
-										<i className="ri-delete-bin-line"></i>
-									</button>
-								</div>
-							</td>
-						</tr>
-						<tr className="border-borderPrimary border-b">
-							<td className="py-6 flex justify-center">
-								<div className="w-14 overflow-hidden rounded-xl">
-									<img
-										src="https://images.unsplash.com/photo-1569718212165-3a8278d5f624?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFtZW58ZW58MHx8MHx8fDA%3D"
-										alt=""
-									/>
-								</div>
-							</td>
-							<td>Fried Rice</td>
-							<td>Food</td>
-							<td>Rp25.000</td>
-							<td>10</td>
-							<td className="h-full">
-								<div className="inline-flex rounded-lg font-normal bg-[#FAFBFD] border border-borderPrimary">
-									<a
-										href="./editProduct.html"
-										className="py-2 px-4 text-black"
-										aria-label="edit"
-									>
-										<i className="ri-edit-box-line"></i>
-									</a>
-									<div className="w-[1px] border-borderPrimary h-10"></div>
-									<button className="py-2 px-4 text-error" aria-label="delete">
-										<i className="ri-delete-bin-line"></i>
-									</button>
-								</div>
-							</td>
-						</tr>
-						<tr className="border-borderPrimary border-b">
-							<td className="py-6 flex justify-center">
-								<div className="w-14 overflow-hidden rounded-xl">
-									<img
-										src="https://images.unsplash.com/photo-1569718212165-3a8278d5f624?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFtZW58ZW58MHx8MHx8fDA%3D"
-										alt=""
-									/>
-								</div>
-							</td>
-							<td>Fried Rice</td>
-							<td>Food</td>
-							<td>Rp25.000</td>
-							<td>10</td>
-							<td className="h-full">
-								<div className="inline-flex rounded-lg font-normal bg-[#FAFBFD] border border-borderPrimary">
-									<a
-										href="./editProduct.html"
-										className="py-2 px-4 text-black"
-										aria-label="edit"
-									>
-										<i className="ri-edit-box-line"></i>
-									</a>
-									<div className="w-[1px] border-borderPrimary h-10"></div>
-									<button className="py-2 px-4 text-error" aria-label="delete">
-										<i className="ri-delete-bin-line"></i>
-									</button>
-								</div>
-							</td>
-						</tr>
-						<tr className="border border-borderPrimary border-b">
-							<td className="py-6 flex justify-center">
-								<div className="w-14 overflow-hidden rounded-xl">
-									<img
-										src="https://images.unsplash.com/photo-1569718212165-3a8278d5f624?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFtZW58ZW58MHx8MHx8fDA%3D"
-										alt=""
-									/>
-								</div>
-							</td>
-							<td>Fried Rice</td>
-							<td>Food</td>
-							<td>Rp25.000</td>
-							<td>10</td>
-							<td className="h-full">
-								<div className="inline-flex rounded-lg font-normal bg-[#FAFBFD] border border-borderPrimary">
-									<a
-										href="./editProduct.html"
-										className="py-2 px-4 text-black"
-										aria-label="edit"
-									>
-										<i className="ri-edit-box-line"></i>
-									</a>
-									<div className="w-[1px] border-borderPrimary h-10"></div>
-									<button className="py-2 px-4 text-error" aria-label="delete">
-										<i className="ri-delete-bin-line"></i>
-									</button>
-								</div>
-							</td>
-						</tr>
-						<tr className="border-borderPrimary border-b">
-							<td className="py-6 flex justify-center">
-								<div className="w-14 overflow-hidden rounded-xl">
-									<img
-										src="https://images.unsplash.com/photo-1569718212165-3a8278d5f624?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmFtZW58ZW58MHx8MHx8fDA%3D"
-										alt=""
-									/>
-								</div>
-							</td>
-							<td>Fried Rice</td>
-							<td>Food</td>
-							<td>Rp25.000</td>
-							<td>10</td>
-							<td className="h-full">
-								<div className="inline-flex rounded-lg font-normal bg-[#FAFBFD] border border-borderPrimary">
-									<a
-										href="./editProduct.html"
-										className="py-2 px-4 text-black"
-										aria-label="edit"
-									>
-										<i className="ri-edit-box-line"></i>
-									</a>
-									<div className="w-[1px] border-borderPrimary h-10"></div>
-									<button className="py-2 px-4 text-error" aria-label="delete">
-										<i className="ri-delete-bin-line"></i>
-									</button>
-								</div>
-							</td>
-						</tr>
+						{data.map((e) => (
+							<tr key={e.id} className="border-borderPrimary border-b">
+								<td className="py-6 flex justify-center">
+									<div className="w-14 overflow-hidden rounded-xl">
+										<img
+											src={e.image}
+											alt=""
+										/>
+									</div>
+								</td>
+								<td>{e.title}</td>
+								<td>{e.category}</td>
+								<td>Rp. {e.price}</td>
+								<td>{e.rating.count}</td>
+								<td className="h-full">
+									<div className="inline-flex rounded-lg font-normal bg-[#FAFBFD] border border-borderPrimary">
+										<Link
+											to="editProduct/123"
+											className="py-2 px-4 text-black"
+											aria-label="edit"
+										>
+											<i className="ri-edit-box-line"></i>
+										</Link>
+										<div className="w-[1px] border-borderPrimary h-10"></div>
+										<button className="py-2 px-4 text-error" onClick={() => deleteData(e.id)} aria-label="delete">
+											<i className="ri-delete-bin-line"></i>
+										</button>
+									</div>
+								</td>
+							</tr>
+						))}
 					</tbody>
 				</table>
 			</div>
--- src/pages/addProduct.jsx
+++ src/pages/addProduct.jsx
@@ -1,12 +1,52 @@
+import { useEffect, useState } from "react";
 import Button from "../components/ui/Button";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 const AddProduct = () => {
+	const [title, setTitle] = useState("");
+	const [category, setCategory] = useState("");
+	const [price, setPrice] = useState(0);
+	const [description, setDescription] = useState("");
+	const [imgLink, setImgLink] = useState("");
+	const [rate, setRate] = useState(0);
+	const [count, setCount] = useState(0);
+
+	const sendData = async () => {
+		try {
+			const response = await axios.post('http://localhost:8000/products', {
+				title: title,
+				category: category,
+				price: price,
+				description: description,
+				image: imgLink,
+				rating: {
+					rate: rate,
+					count: count,
+				}
+			});
+
+			console.log(response.data);
+
+			if (response.status === 200) {
+				Navigate('/products')
+			} else {
+				console.log("gagal")
+			}
+
+		} catch (err) {
+			console.error(err.message);
+		}
+	}
+
 	return (
 		<>
 			<div className="flex justify-between items-center">
 				<p className="text-3xl font-bold">Add Products</p>
 				<div className="flex gap-2">
-					<Button>
+					<Button
+						onClick={sendData}
+					>
 						<a href="">
 							<p className="-mb-[2px]">Add</p>
 						</a>
@@ -29,6 +69,8 @@ const AddProduct = () => {
 					<div className="flex flex-col gap-3">
 						<label htmlFor="product_name">Product Name</label>
 						<input
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
 							type="text"
 							id="product_name"
 							name="product_name"
@@ -36,51 +78,24 @@ const AddProduct = () => {
 							className="px-5 py-2 border-borderPrimary border bg-backgroundPrimary rounded-md"
 						/>
 					</div>
-					<div>
-						<fieldset className="flex flex-col gap-3">
-							<p>Category</p>
-							<div className="flex gap-1">
-								<div>
-									<input
-										type="radio"
-										name="category"
-										id="food"
-										className="hidden peer"
-										value="food"
-									/>
-									<label
-										role="button"
-										htmlFor="food"
-										tabIndex="0"
-										className="peer-checked:text-white bg-[#D9D9D9] peer-checked:bg-primary px-6 py-2 text-base rounded-s-md"
-									>
-										Food
-									</label>
-								</div>
-								<div>
-									<input
-										type="radio"
-										name="category"
-										id="drink"
-										className="hidden peer"
-										value="drink"
-									/>
-									<label
-										role="button"
-										htmlFor="drink"
-										tabIndex="0"
-										className="peer-checked:text-white bg-[#D9D9D9] peer-checked:bg-primary px-6 py-2 text-base rounded-e-md"
-									>
-										Drink
-									</label>
-								</div>
-							</div>
-						</fieldset>
+					<div className="flex flex-col gap-3">
+						<label htmlFor="kategori">Kategori</label>
+						<input
+							value={category}
+							onChange={(e) => setCategory(e.target.value)}
+							type="text"
+							id="kategori"
+							name="kategori"
+							placeholder="Input kategori"
+							className="px-5 py-2 border-borderPrimary border bg-backgroundPrimary rounded-md"
+						/>
 					</div>
 					<div className="flex flex-col gap-3">
 						<label htmlFor="price">Price</label>
 						<input
-							type="text"
+							value={price}
+							onChange={(e) => setPrice(e.target.value)}
+							type="number"
 							id="price"
 							name="price"
 							placeholder="Input price"
@@ -88,12 +103,49 @@ const AddProduct = () => {
 						/>
 					</div>
 					<div className="flex flex-col gap-3">
-						<label htmlFor="stock">Stock</label>
+						<label htmlFor="deskripsi">Deskripsi</label>
 						<input
+							value={description}
+							onChange={(e) => setDescription(e.target.value)}
 							type="text"
-							id="stock"
-							name="stock"
-							placeholder="Input stock"
+							id="deskripsi"
+							name="deskripsi"
+							placeholder="Input deskripsi"
+							className="px-5 py-2 border-borderPrimary border bg-backgroundPrimary rounded-md"
+						/>
+					</div>
+					<div className="flex flex-col gap-3">
+						<label htmlFor="gambar">Gambar</label>
+						<input
+							value={imgLink}
+							onChange={(e) => setImgLink(e.target.value)}
+							type="text"
+							id="gambar"
+							name="gambar"
+							placeholder="URL gambar"
+							className="px-5 py-2 border-borderPrimary border bg-backgroundPrimary rounded-md"
+						/>
+					</div>
+					<div className="flex flex-col gap-3">
+						<label htmlFor="rate">Rate</label>
+						<input
+							value={rate}
+							onChange={(e) => setRate(e.target.value)}
+							type="number"
+							id="rate"
+							name="rate"
+							placeholder="Input rate"
+							className="px-5 py-2 border-borderPrimary border bg-backgroundPrimary rounded-md"
+						/>
+					</div><div className="flex flex-col gap-3">
+						<label htmlFor="stok">Stok</label>
+						<input
+							value={count}
+							onChange={(e) => setCount(e.target.value)}
+							type="number"
+							id="stok"
+							name="stok"
+							placeholder="Input stok"
 							className="px-5 py-2 border-borderPrimary border bg-backgroundPrimary rounded-md"
 						/>
 					</div>
